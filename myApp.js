7e7ed1ddad7bf95c7a00a4bc0dcf3e19d5676738
JR401 <joramiranda8@gmail.com>
2024-11-20 08:05:08 -0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,13 +4,13 @@ console.log("Hello World")
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
-// Normal usage
-app.use(express.static(__dirname + "/public"));
 
-// Assets at the /public route
+app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
 
+app.use(express.static(__dirname + "/json"));
+app.use("/json", express.static(__dirname + "/public"))
 
 
 
